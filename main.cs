0ad9c3699802514e58936ee611eea86cf91f1c67
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-18 01:06:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,29 @@
+// Created by: Claire Bedrossian
+// Created on: Oct 2022
+//
+// This program calculates area and perimeter of a square
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int sideA;
+        int perimeter;
+
+        Console.WriteLine("This program calculates the area and perimeter of a square.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the length of side A (cm): ");
+        sideA = Convert.ToInt32(Console.ReadLine());
+
+        perimeter = sideA * 4;
+
+        Console.WriteLine("");
+        Console.WriteLine("The perimeter is: " + perimeter + " cm. ");
+
+        Console.WriteLine("\nDone.");
+    }
+}
